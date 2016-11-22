829f68099a14e38a6b9e30c5bd6cac34de34b85e
Neil <nruggier@gmail.com>
2016-11-22 15:14:04 -0500

    Add family, colors and phrases.  Condense layouts to just word_list.xml

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -2,23 +2,16 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.ArrayAdapter;
-import android.widget.LinearLayout;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import java.util.ArrayList;
-
-import static com.example.android.miwok.R.id.numbers;
-import static com.example.android.miwok.R.id.list;
 
 public class NumbersActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_numbers);
+        setContentView(R.layout.word_list);
 
         ArrayList<Word> words = new ArrayList<Word>();
 
--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -17,6 +17,7 @@ import java.util.ArrayList;
 
 public class WordAdapter extends ArrayAdapter<Word> {
 
+    //constructor
     public WordAdapter(Activity context, ArrayList<Word> words) {
         super(context, 0, words);
     }
@@ -24,23 +25,25 @@ public class WordAdapter extends ArrayAdapter<Word> {
     @NonNull
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-
+        //MAIN GOAL: Get listItemView, either from recycling, or from by inflating a new one
+        //second input param is a recycled view ^^^.  If one is passed, it means it can be recycled
         View listItemView = convertView;
+        //if null, means no recycled views to be used yet.  None passed to this method
         if(listItemView == null) {
-            listItemView = LayoutInflater.from(getContext()).inflate(
-                    R.layout.list_item, parent, false);
+            listItemView = LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent, false); //create new listItemView
         }
 
         Word currentWord = getItem(position);
 
+        //set miwok word in display
         TextView nameTextView = (TextView) listItemView.findViewById(R.id.miwok_text_view);
-
         nameTextView.setText(currentWord.getMiwokWord());
 
+        //set default word in display
         TextView numberTextView = (TextView) listItemView.findViewById(R.id.default_text_view);
-
         numberTextView.setText(currentWord.getDefaultWord());
 
+        //return the view
         return listItemView;
     }
 }
